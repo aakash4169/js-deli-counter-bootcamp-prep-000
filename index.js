ffd41e0e51cd078cf8103c431231c9b8a19d540f
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-05-29 04:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine,person){
   katzDeliLine.push(person);
-//  var n=katzDeliLine.indexOf(person)+1;
-var n=katzDeliLine.length;
+  var n=katzDeliLine.indexOf(person)+1;
+//var n=katzDeliLine.length;
   var str=`Welcome, ${person}. You are number ${n} in line`;
 //var str= "Welcome, "+person+". You are number "+n+" in line"; 
 return str;
